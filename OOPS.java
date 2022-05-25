cffaf12622e02476e69034d196d6ea9729efdefc
DheerajSHSA <Dheerajkashyap4889@gmail.com>
2022-05-25 15:39:14 +0530

    Notes in Readme

--- OOPS.java
+++ OOPS.java
@@ -22,14 +22,28 @@ class Student
         System.out.println("Student's Name is "+this.name);
         System.out.println("Student's Age is "+this.age);
     }
+
+    Student(Student s2)
+    {
+        this.name = s2.name;
+        this.age = s2.age;
+
+    }
+
+    Student()
+    {
+
+    }
 }
 public class OOPS {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.name = "Sharadha";
+        s1.name = "Aman";
         s1.age = 24;
 
-        s1.printInfo();
+        Student s2 = new Student(s1);
+        s2.printInfo();
+
 
 
     }
---
+++ Readme.md
@@ -0,0 +1,6 @@
+OOP's concept:
+
+1.Abstraction
+2.Encapsulation
+3.Inheritance
+4.Polymorphism
